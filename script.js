32b210e0dd83d5acba5d3d1c15641f18a4a0af2c
Lmcmax <lmcmax99@gmail.com>
2025-03-26 04:26:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// testing .....
+let x = "i am a Hacker ."
+const y = 1
+
+function lmc(a,b,c=12) {
+    console.log(x , y)
+    console.log("try to kncock me ...🚀")
+    console.log(String(a)+" ==> " , b+c)
+    console.log("\t This web sight is 🎨 updateing ...")
+    return 0;
+}
+
+console.log(lmc("\t(^_^ )",1 , 2))
+
+
+
+//😃 😊 😎 🤖 😍 🤔 🤯 🤓 😈 🤡 🧐 😱 😂 🤭 😤 🥶 🥳 😵 😇 🤩
+
+// i =["😃" , "😎" ,"😊"]
+
+// for(x in i){
+//     console.log(i[x])
+// }
+
+function alerting() {
+    alert("🔐 This websight is want to be updated ...😈 ")
+    console.log("\t\n\t==> 404")
+}
+
+// for(let i = 100 ; i < 10 ; i++ , sum = 0 ){
+//     sum = sum + i
+//     console.log(sum)
+// }
+
+
+
+
+
+// tested <<<<<<<<<<<<<
+
+function what() {
+    var min = 1 ;
+    var max = 23 ;
+    i =["😃","😃", "😊", "😎", "🤖", "😍", "🤔", "🤯", "🤓", "😈", "🤡", "🧐", "😱", "😂", "🤭", "😤", "🥶", "🥳", "😵", "😇","😃","😃", "🤩" ,"💪"]
+    imogi = i[(Math.floor(Math.random() * (max - min + 1)) + min )]
+    var a = prompt("Enter password : ???")
+
+    if (String(a) == "ok") {
+        window.open(`test.html# ${imogi}`)
+        console.log("ok 🎨")
+    } else {
+        console.log("Nothing ... (^_^ ) 😊")
+    }
+    return 0 ;
+}
+
+
+////////////////////////////////DOM///////////////////////////////////
+
+// const paragraph = document.createElement("kbd")
+// paragraph.textContent = "Hi iam Labib ."
+
+// document.body.append(paragraph)
